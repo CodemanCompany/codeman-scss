6e18ca23ad6e7d9afe75c382b5e86dbd36c85e3c
Gustavo Martinez <gustavo@codeman.company>
2024-06-19 20:32:23 -0600

    Progress Linux

--- scss/app.sh
+++ scss/app.sh
@@ -6,4 +6,12 @@
 # ╚██████╗╚██████╔╝██████╔╝███████╗██║ ╚═╝ ██║██║  ██║██║ ╚████║
 #  ╚═════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝
 
+
 sass --style compressed --watch scss/app.scss:css/styles.min.css
+
+# In a container you can use the following command (Only in server for development)
+
+npm install -g nodemon
+npm install -g sass
+
+nodemon --watch scss --ext scss --exec "sass --style compressed scss/app.scss:css/styles.min.css" &
